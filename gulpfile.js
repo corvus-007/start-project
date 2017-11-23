1ce9dcf779bb14d4f8d6de115bbb7701fa8d34ff
Denis <corvus007@yandex.ru>
2017-11-23 12:40:43 +0300

    Создает папку modules и перемещает модули из папки plugins в нее

--- gulpfile.js
+++ gulpfile.js
@@ -15,113 +15,148 @@ var concat = require('gulp-concat');
 var run = require('run-sequence');
 var del = require('del');
 
-
-gulp.task('style', function () {
-  return gulp.src('app/scss/style.scss')
-    .pipe(plumber({
-      errorHandler: function (err) {
-        console.log(err);
-      }
-    }))
-    .pipe(sass.sync({
-      outputStyle: 'expanded'
-    }).on('error', sass.logError))
-    .pipe(postcss([
-      autoprefixer({
-        browsers: [
-          'last 2 version'
-        ]
-      }),
-      mqpacker({
-        sort: true
-      })
-    ]))
-    // .pipe(minify())
-    .pipe(gulp.dest('build/'))
-    .pipe(browserSync.stream());
+gulp.task('style', function() {
+  return (gulp
+      .src('app/scss/style.scss')
+      .pipe(
+        plumber({
+          errorHandler: function(err) {
+            console.log(err);
+          }
+        })
+      )
+      .pipe(
+        sass
+          .sync({
+            outputStyle: 'expanded'
+          })
+          .on('error', sass.logError)
+      )
+      .pipe(
+        postcss([
+          autoprefixer({
+            browsers: ['last 2 version']
+          }),
+          mqpacker({
+            sort: true
+          })
+        ])
+      )
+      // .pipe(minify())
+      .pipe(gulp.dest('build/'))
+      .pipe(browserSync.stream()) );
 });
 
-gulp.task('plugins-js', function () {
-  gulp.src('app/js/plugins/*.js')
+gulp.task('plugins-js', function() {
+  gulp
+    .src('app/js/plugins/*.js')
     .pipe(concat('plugins.js'))
     .pipe(uglify())
     .pipe(gulp.dest('build/js'))
     .pipe(browserSync.stream());
 });
 
-gulp.task('copy-script', function () {
-  gulp.src(['app/js/*.{js,json}', '!app/js/plugins/**'])
+gulp.task('modules-js', function() {
+  gulp
+    .src([
+      'app/js/modules/util.js',
+      'app/js/modules/searchbar.js',
+      'app/js/modules/navbar.js',
+      'app/js/modules/out-cover.js',
+      'app/js/modules/main-nav.js'
+    ])
+    .pipe(concat('modules.js'))
     .pipe(gulp.dest('build/js'))
     .pipe(browserSync.stream());
 });
 
-gulp.task('fileinclude', function () {
-  gulp.src('app/*.html')
-    .pipe(fileinclude({
-      indent: true
-    }))
+gulp.task('copy-script', function() {
+  gulp
+    .src(['app/js/*.{js,json}', '!app/js/plugins/**'])
+    .pipe(gulp.dest('build/js'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('fileinclude', function() {
+  gulp
+    .src('app/*.html')
+    .pipe(
+      fileinclude({
+        indent: true
+      })
+    )
     .pipe(gulp.dest('build'));
 });
 
-gulp.task('copy-images', function () {
-  return gulp.src('app/images/**/*')
-    .pipe(gulp.dest('build/images'));
+gulp.task('copy-images', function() {
+  return gulp.src('app/images/**/*').pipe(gulp.dest('build/images'));
 });
 
-
-gulp.task('symbols', function () {
-  return gulp.src('build/images/svg-symbols/*.svg')
+gulp.task('symbols', function() {
+  return gulp
+    .src('build/images/svg-symbols/*.svg')
     .pipe(svgmin())
-    .pipe(svgstore({
-      inlineSvg: true
-    }))
+    .pipe(
+      svgstore({
+        inlineSvg: true
+      })
+    )
     .pipe(rename('symbols.svg'))
     .pipe(gulp.dest('build/images'));
 });
 
-gulp.task('clean', function () {
+gulp.task('clean', function() {
   return del('build');
 });
 
-gulp.task('copy', function () {
-  return gulp.src([
-      'app/fonts/**/*.{woff,woff2}',
-      // 'app/images/**',
-      //      'app/js/**',
-      'app/*.html'
-    ], {
-      base: 'app',
-      allowEmpty: true
-    })
+gulp.task('copy', function() {
+  return gulp
+    .src(
+      [
+        'app/fonts/**/*.{woff,woff2}',
+        // 'app/images/**',
+        //      'app/js/**',
+        'app/*.html'
+      ],
+      {
+        base: 'app',
+        allowEmpty: true
+      }
+    )
     .pipe(gulp.dest('build'));
 });
 
-gulp.task('build', function (fn) {
+gulp.task('build', function(fn) {
   run(
     'clean',
     'copy',
     'style',
     'plugins-js',
+    'modules-js',
     'copy-script',
     'fileinclude',
     'copy-images',
     'symbols',
-    fn);
+    fn
+  );
 });
 
-gulp.task('serve', function () {
+gulp.task('serve', function() {
   browserSync.init({
-    server: "./build"
+    server: './build'
   });
 
-  gulp.watch('app/scss/**/*.scss', function () {
-    setTimeout(function () {
+  gulp.watch('app/scss/**/*.scss', function() {
+    setTimeout(function() {
       gulp.start('style');
     }, 500);
   });
   gulp.watch('app/images/**/*', ['copy-images']);
   gulp.watch('build/images/svg-symbols/*.svg', ['symbols']);
   gulp.watch('app/js/plugins/*.js', ['plugins-js']);
-  gulp.watch(['app/js/*.{js,json}', '!app/js/plugins/**'], ['copy-script']);
-  gulp.watch(['app/*.html', 'app/blocks/**/*.html'], ['fileinclude']).on('change', browserSync.reload);
+  gulp.watch('app/js/modules/*.js', ['modules-js']);
+  gulp.watch('app/js/*.{js,json}', ['copy-script']);
+  gulp
+    .watch(['app/*.html', 'app/blocks/**/*.html'], ['fileinclude'])
+    .on('change', browserSync.reload);
 });
