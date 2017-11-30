74fa88c263b4e53e4662cbfae3b99839f48197bb
Denis <corvus007@yandex.ru>
2017-11-30 11:33:35 +0300

    Копирование шрифтов и html файлов разбивает на задачи. перименовывает задачу symbols -> make-symbols

--- gulpfile.js
+++ gulpfile.js
@@ -58,13 +58,7 @@ gulp.task('plugins-js', function() {
 
 gulp.task('modules-js', function() {
   gulp
-    .src([
-      'app/js/modules/util.js',
-      'app/js/modules/searchbar.js',
-      'app/js/modules/navbar.js',
-      'app/js/modules/out-cover.js',
-      'app/js/modules/main-nav.js'
-    ])
+    .src(['app/js/modules/util.js'])
     .pipe(concat('modules.js'))
     .pipe(gulp.dest('build/js'))
     .pipe(browserSync.stream());
@@ -92,7 +86,7 @@ gulp.task('copy-images', function() {
   return gulp.src('app/images/**/*').pipe(gulp.dest('build/images'));
 });
 
-gulp.task('symbols', function() {
+gulp.task('make-symbols', function() {
   return gulp
     .src('build/images/svg-symbols/*.svg')
     .pipe(svgmin())
@@ -105,38 +99,30 @@ gulp.task('symbols', function() {
     .pipe(gulp.dest('build/images'));
 });
 
-gulp.task('clean', function() {
-  return del('build');
+gulp.task('copy-fonts', function() {
+  return gulp.src('app/fonts/**/*.{woff,woff2}').pipe(gulp.dest('build'));
 });
 
-gulp.task('copy', function() {
-  return gulp
-    .src(
-      [
-        'app/fonts/**/*.{woff,woff2}',
-        // 'app/images/**',
-        //      'app/js/**',
-        'app/*.html'
-      ],
-      {
-        base: 'app',
-        allowEmpty: true
-      }
-    )
-    .pipe(gulp.dest('build'));
+gulp.task('copy-root-htmls', function() {
+  return gulp.src('app/*.html').pipe(gulp.dest('build'));
+});
+
+gulp.task('clean', function() {
+  return del('build');
 });
 
 gulp.task('build', function(fn) {
   run(
     'clean',
-    'copy',
+    'copy-root-htmls',
+    'copy-fonts',
+    'copy-images',
+    'copy-script',
     'style',
     'plugins-js',
     'modules-js',
-    'copy-script',
     'fileinclude',
-    'copy-images',
-    'symbols',
+    'make-symbols',
     fn
   );
 });
@@ -151,11 +137,12 @@ gulp.task('serve', function() {
       gulp.start('style');
     }, 500);
   });
+  gulp.watch('app/fonts/**/*', ['copy-fonts']);
   gulp.watch('app/images/**/*', ['copy-images']);
-  gulp.watch('build/images/svg-symbols/*.svg', ['symbols']);
+  gulp.watch('app/js/*.{js,json}', ['copy-script']);
+  gulp.watch('build/images/svg-symbols/*.svg', ['make-symbols']);
   gulp.watch('app/js/plugins/*.js', ['plugins-js']);
   gulp.watch('app/js/modules/*.js', ['modules-js']);
-  gulp.watch('app/js/*.{js,json}', ['copy-script']);
   gulp
     .watch(['app/*.html', 'app/blocks/**/*.html'], ['fileinclude'])
     .on('change', browserSync.reload);
